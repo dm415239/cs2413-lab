fd9d531adbc7c1cdd8b530346f15bc3a5b9ae4db
dm415239 <dmccay@ttu.edu>
2026-04-09 22:23:08 -0500

    finish lab 5 and 6

--- lab06/Question2/student.c
+++ lab06/Question2/student.c
@@ -1,33 +1,23 @@
 #include "graph.h"
-// Description
-
-// Write a function that counts how many nodes are isolated.
-// A node is isolated if it has no edges connected to it.
-// // The graph is stored as an adjacency matrix. 
-// Graph* g is a pointer to a Graph struct, which contains a 2D array adj[MAX_NODES][MAX_NODES]. 
-// The value at adj[u][v] is 1 if there is an edge between nodes u and v, and 0 otherwise.
-
-
-
 
 int count_isolated(Graph* g) {
-    // TODO: implement
-    // return -1;
-   
+    if (!g) return 0;
+
+    int isolated_count = 0;
+
+    for (int i = 0; i < MAX_NODES; i++) {
+        int has_edge = 0;
+        for (int j = 0; j < MAX_NODES; j++) {
+            // Skip self-loop check (usually diagonal is 0)
+            if (i != j && g->adj[i][j] == 1) {
+                has_edge = 1;
+                break;
+            }
+        }
+        if (!has_edge) {
+            isolated_count++;
+        }
+    }
+
+    return isolated_count;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Tip: In the adjacency matrix, that means the entire row for that node contains only 0s.
--- lab06/Question3/student.c
+++ lab06/Question3/student.c
@@ -1,13 +1,18 @@
 #include "graph.h"
 
-// Description
-
-// Check whether the graph contains a triangle. A triangle is a set of three nodes that are all connected to each other. 
-// The graph is stored as an adjacency matrix. 
-// Graph* g is a pointer to a Graph struct, which contains a 2D array adj[MAX_NODES][MAX_NODES]. 
-// The value at adj[u][v] is 1 if there is an edge between nodes u and v, and 0 otherwise.
-
 int has_triangle(Graph* g) {
-    // TODO: implement
-    // return -1;
+    if (!g) return 0;
+
+    // Check all triples of distinct nodes (i < j < k)
+    for (int i = 0; i < MAX_NODES; i++) {
+        for (int j = i + 1; j < MAX_NODES; j++) {
+            if (g->adj[i][j] == 0) continue; // edge i-j missing, skip this pair
+            for (int k = j + 1; k < MAX_NODES; k++) {
+                if (g->adj[i][k] == 1 && g->adj[j][k] == 1) {
+                    return 1; // triangle found
+                }
+            }
+        }
+    }
+    return 0; // no triangle found
 }
