899d5c7eeb3b2f177cb13c514c57d56079ceae60
dm415239 <dmccay@ttu.edu>
2026-05-11 23:37:02 -0500

    finish asighnment04

--- lab07/Question5/student.c
+++ lab07/Question5/student.c
@@ -1,28 +1,15 @@
-/*
-Question 5: Selection Sort
-
-Description:
-Implement selection sort for an integer array.
-
-Selection sort repeatedly finds the smallest element from the unsorted
-portion of the array and swaps it with the first element of the unsorted
-portion.
-
-The function should sort the array in ascending order.
-
-Example:
-Input:  [5, 1, 4, 2, 8]
-Output: [1, 2, 4, 5, 8]
-
-Notes:
-- If the array is empty or has only one element, do nothing.
-- You may write a helper function such as swap(...) if you want.
-- Do not use any built-in sorting function.
-*/
-
 void selectionSort(int arr[], int size) {
-    // TODO: implement selection sort
-    (void)arr;
-    (void)size;
+    if (size < 2) return;
+    for (int i = 0; i < size - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+        if (minIdx != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
 }
-
--- lab07/Question7/student.c
+++ lab07/Question7/student.c
@@ -3,27 +3,7 @@
 
 /*
 Hybrid Merge Sort Lab
-
-Objective:
-Implement a hybrid sorting algorithm that combines merge sort and insertion sort.
-
-Idea:
-- Merge sort has worst-case time complexity O(n log n).
-- Insertion sort has worst-case time complexity O(n^2), but it is fast for small subarrays.
-- Therefore, when the current subarray size is less than or equal to k,
-  use insertion sort instead of continuing recursive merge sort.
-
-Input array:
-[39, 9, 81, 45, 90, 27, 72, 18]
-
-Expected sorted output:
-[9, 18, 27, 39, 45, 72, 81, 90]
-
-Students need to complete:
-
-1. insertionSort
-2. merge
-3. hybridMergeSort
+...
 */
 
 /*
@@ -34,7 +14,6 @@ void printArray(int arr[], int size) {
     printf("[");
     for (int i = 0; i < size; i++) {
         printf("%d", arr[i]);
-
         if (i < size - 1) {
             printf(" ");
         }
@@ -59,33 +38,73 @@ int isSorted(int arr[], int size) {
 Sort arr[left...right] using insertion sort.
 */
 void insertionSort(int arr[], int left, int right) {
-    // TODO: implement insertion sort for arr[left...right]
+    for (int i = left + 1; i <= right; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= left && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 /*
 Merge two sorted subarrays into one sorted subarray.
 */
 void merge(int arr[], int left, int mid, int right) {
-    // TODO: implement merge operation
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int* L = (int*)malloc(n1 * sizeof(int));
+    int* R = (int*)malloc(n2 * sizeof(int));
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+
+    free(L);
+    free(R);
 }
 
 /*
 Hybrid merge sort.
 */
 void hybridMergeSort(int arr[], int left, int right, int k) {
-    // TODO: implement hybrid merge sort
+    if (left >= right)
+        return;
+
+    if (right - left + 1 <= k) {
+        insertionSort(arr, left, right);
+    }
+    else {
+        int mid = left + (right - left) / 2;
+        hybridMergeSort(arr, left, mid, k);
+        hybridMergeSort(arr, mid + 1, right, k);
+        merge(arr, left, mid, right);
+    }
 }
 
 int main() {
-    int arr[] = {39, 9, 81, 45, 90, 27, 72, 18};
+    int arr[] = { 39, 9, 81, 45, 90, 27, 72, 18 };
     int size = sizeof(arr) / sizeof(arr[0]);
 
     /*
     k is the threshold.
-
     When the current subarray size is less than or equal to k,
     insertion sort should be used.
-
     You may change k to test different behaviors.
     */
     int k = 3;
@@ -100,7 +119,8 @@ int main() {
 
     if (isSorted(arr, size)) {
         printf("Result: PASS\n");
-    } else {
+    }
+    else {
         printf("Result: FAIL\n");
     }
 
